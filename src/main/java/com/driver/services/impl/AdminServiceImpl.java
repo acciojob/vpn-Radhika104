0849123b16c4778b56440dfc715bc056178b9a5c
Radhika104 <87032257+Radhika104@users.noreply.github.com>
2023-06-24 21:58:04 +0530

    checking admin

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
 import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
 import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
@@ -8,6 +10,9 @@ import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -42,6 +47,19 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-        return null;
+        List<String> list= Arrays.asList(new String[]{"AUS","IND","USA","CHI","JPN"});
+        if(!list.contains(countryName.toUpperCase())) throw new Exception("Country not found");
+        ServiceProvider serviceProvider=serviceProviderRepository1.findById(serviceProviderId).get();
+
+        Country country=new Country();
+        CountryName countryName1=CountryName.valueOf(countryName.toUpperCase());
+        country.setCountryName(countryName1);
+        country.setCode(countryName1.toCode());
+        country.setUser(null);
+        country.setServiceProvider(serviceProvider);
+
+        serviceProvider.getCountryList().add(country);
+        serviceProviderRepository1.save(serviceProvider);
+        return serviceProvider;
     }
 }
